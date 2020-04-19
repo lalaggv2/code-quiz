80345c77c0a64922217feb0a41a3602b408fb0a1
Lala <lalagg@hotmail.com>
2020-04-19 00:11:18 -0400

    setting up questions

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,23 @@
-var
+var quizQuestions = [{
+
+    question: "What are the variables used for in JS?",
+    answers: {
+        a: "Store values",
+        b: "Things that change",
+        c: "To do stuff",
+        d: "All of the above"
+    },
+    correctAnswer: "a"
+},
+{
+    question: "Why do you use JS in your website?",
+    answers: {}
+        a: "To make it fancy",
+    b: "To allow user input",
+    c: "To make it dynamic",
+    d: "All of the above"
+},
+    correctAnswer: "d"
+},
+
+}]
