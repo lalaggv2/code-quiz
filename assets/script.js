659f6956879142304403cdcc1715d771927067ea
Lala <lalagg@hotmail.com>
2020-04-19 21:00:00 -0400

    writing questions

--- assets/script.js
+++ assets/script.js
@@ -11,13 +11,46 @@ var quizQuestions = [{
 },
 {
     question: "Why do you use JS in your website?",
-    answers: {}
+    answers: {
         a: "To make it fancy",
-    b: "To allow user input",
-    c: "To make it dynamic",
-    d: "All of the above"
-},
+        b: "To allow user input",
+        c: "To make it dynamic",
+        d: "All of the above"
+    },
     correctAnswer: "d"
 },
+{
+    question: "Which one of this is correct?",
+    answers: {
+        a: "var myVariable=4",
+        b: "var myVariable = '4kdkfldlkfs'",
+        c: "var myVariable = car;",
+        d: "var myVariable = car",
+    },
+    correctAnswer: "d"
+},
+{
+    question: "What should you do when faced with a new JS task?",
+    answers: {
+        a: "Dive right in to code, things will work out in the end",
+        b: "Try to understand what you need to do",
+        c: "Search online for answers",
+        d: "Panic and feel overwhelmed",
+    }
+    correctAnswer: "b"
+},
+{
+    question: "What is console?",
+    answers: {
+        a: "A thing I use to play video games",
+        b: "Where my mom showcases her figurines",
+        c: "Part of the Inspector used to check my JS",
+        d: "Conforting someone at a time of grief",
+    }
+    correctAnswer: "c"
+},
 
-}]
+];
+console.log(quizQuestions);
+
+var showQuestions = 
